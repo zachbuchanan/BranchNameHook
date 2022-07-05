547dfdec2d8db187ec8b904b8021c56f29e9bc41
Zach <zbuchanan@alarm.com>
2022-07-05 13:30:26 -0400

    develop: branch name is being added to message

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -1,41 +1,9 @@
 #!/bin/sh
-# This way you can customize which branches should be skipped when
-# prepending commit message.
-RED="\033[1;31m"
-GREEN="\033[1;32m"
-ORANGE="\033[0;33m"
-NOCOLOR="\033[0m"
+branchPath=$(git symbolic-ref -q HEAD) #Somthing like refs/heads/myBranchName
+branchName=${branchPath##*/}      #Get text behind the last / of the branch path
 
-# disable if on these
-if [ -z "$BRANCHES_TO_SKIP" ]; then
-    BRANCHES_TO_SKIP=(master main develop staging test)
-fi
+firstLine=$(head -n1 $1)
 
-AUTHORINFO=$(git var GIT_AUTHOR_IDENT) || exit 1
-NAME=$(printf '%s\n\n' "${AUTHORINFO}" | sed -n 's/^\(.*\) <.*$/\1/p')
-
-# Regex to check the valid branch name
-VALID_BRANCH_REGEX="(^(feature|hotfix|bugfix|release|dev|improvement))|^([A-Z]+\-[0-9]+)$"
-
-# get current branch name
-BRANCH_NAME=$(git branch | grep '*' \ sed 's/* //')
-BRANCH_EXCLUDED=$(printf "%s/n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
-
-# A developer has already prepended the commit in the format BRANCH_NAME
-BRANCH_IN_COMMIT=$(grep -c "$BRANCH_NAME" $1)
-
-# check the branch name is valid or not
-if [[ "$BRANCH_NAME" =~ $VALID_BRANCH_REGEX ]]; then
-  # if face any error in mac then run chmod u+x .git/hooks/prepare-commit-msg and restart your terminal
-  if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then 
-    sed -i.bak -e "1s/^/[$BRANCH_NAME] /" $1
-  fi
-else
-  echo -e "\n${RED}Please correct the branch name${NOCOLOR}"
-  printf "\nBranch Name not as per the defined rules like:  "
-  echo -e "${GREEN}dev, hotfix, bugfix, release, dev, improvement, hotfix-102\n"
-  echo -e "${ORANGE}You cannot push in these branches directly: ${BRANCHES_TO_SKIP[@]}\n"
-  echo -e "${ORANGE}Current BRANCH_NAME: ${BRANCH_NAME}"
-  echo -e "\n${RED}Oh, please stop. I cannot allow you to commit with your current branch: ${BRANCH_NAME}${NOCOLOR}"
-  exit 1;
+if [ -z "$firstLine"  ] ;then #Check that this is not an amend by checking that the first line is empty
+    sed -i "1s/^/$branchName: \n/" $1 #Insert branch name at the start of the commit message file
 fi
