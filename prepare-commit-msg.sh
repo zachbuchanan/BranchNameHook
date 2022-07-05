a019ec4e5b2559640dda3f6aa06472d511cf1965
Zach <zbuchanan@alarm.com>
2022-07-05 14:00:18 -0400

    trying new script

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -1,24 +1,45 @@
 #!/bin/sh
 
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+# This way you can customize which branches should be skipped when
+# prepending commit message.
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+ORANGE="\033[0;33m"
+NOCOLOR="\033[0m"
 
-# Ensure BRANCH_NAME is not empty and is not in a detached HEAD state (i.e. rebase).
-# SKIP_PREPARE_COMMIT_MSG may be used as an escape hatch to disable this hook,
-# while still allowing other githooks to run.
+if [ ! -z "$BRANCH_NAME" ] && [ "$BRANCH_NAME" != "HEAD" ] && [ "$SKIP_PREPARE_COMMIT_MSG" != 1 ]; then
 
-if [ ! -z "$BRANCH_NAME" ] && [ "$BRANCH_NAME" != "HEAD" ] && [ "$SKIP_PREPARE_COMMIT_MSG" != 1 ]; then # add this?
-
-  PREFIX_PATTERN='[A-Z]{2,5}-[0-9]{1,4}'
-
-  [[ $BRANCH_NAME =~ $PREFIX_PATTERN ]]
-
-  PREFIX=${BASH_REMATCH[0]}
-
-  PREFIX_IN_COMMIT=$(grep -c "\[$PREFIX\]" $1)
-
-  # Ensure PREFIX exists in BRANCH_NAME and is not already present in the commit message
-  if [[ -n "$PREFIX" ]] && ! [[ $PREFIX_IN_COMMIT -ge 1 ]]; then
-    sed -i.bak -e "1s/^/[$PREFIX] ~" $1
+  if [ -z "$BRANCHES_TO_SKIP" ]; then
+    BRANCHES_TO_SKIP=(master production staging main)
   fi
 
+  AUTHORINFO=$(git var GIT_AUTHOR_IDENT) || exit 1
+  NAME=$(printf '%s\n\n' "${AUTHORINFO}" | sed -n 's/^\(.*\) <.*$/\1/p')
+  # Regex to check the valid branch name
+  VALID_BRANCH_REGEX="(^(feature|hotfix|bugfix|release|dev|improvement|TICKET))|^([A-Z]+\-[0-9]+)$"
+
+  # Get branch name and description
+  BRANCH_NAME=$(git branch | grep '*' | sed 's/* //')
+
+  # Branch name should be excluded from the prepend
+  BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
+
+  # A developer has already prepended the commit in the format BRANCH_NAME
+  BRANCH_IN_COMMIT=$(grep -c "$BRANCH_NAME" $1)
+
+  # check the branch name is valid or not
+  if [[ "$BRANCH_NAME" =~ $VALID_BRANCH_REGEX ]]; then
+    # if face any error in mac then run chmod u+x .git/hooks/prepare-commit-msg and restart your terminal
+    if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then 
+      sed -i.bak -e "1s/^/[$BRANCH_NAME] /" $1
+    fi
+  else
+    echo -e "\n${RED}Please correct the branch name${NOCOLOR}"
+    printf "\nBranch Name not as per the defined rules like:  "
+    echo -e "${GREEN}dev, hotfix, bugfix, release, dev, improvement, hotfix-102\n"
+    echo -e "${ORANGE}You cannot push in these branches directly: ${BRANCHES_TO_SKIP[@]}\n"
+    echo -e "${ORANGE}Current BRANCH_NAME: ${BRANCH_NAME}"
+    echo -e "\n${RED}Oh, please stop. I cannot allow you to commit with your current branch: ${BRANCH_NAME}${NOCOLOR}"
+    exit 1;
+  fi
 fi
